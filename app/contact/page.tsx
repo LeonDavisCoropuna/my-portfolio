edbc0ec5a56d7c25990e8b8da6a6acc949a4f587
Leon Davis <ldavis@unsa.edu.pe>
2024-12-29 14:24:45 -0500

    fix eslint

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -37,9 +37,8 @@ const Contact = () => {
         <div className='flex flex-col xl:flex-row gap-[30px]'>
           {/* form */}
           <div className='xl:h-[54%] order-2 xl:order-none'>
-
             <form className='flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl'>
-              <h3 className='text-4xl text-accent'>Let'swork together</h3>
+              <h3 className='text-4xl text-accent'>Let's work together</h3>
               <p className='text-white/60'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt, blanditiis!</p>
               {/* input */}
               <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
